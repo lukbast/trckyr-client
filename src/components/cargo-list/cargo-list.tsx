794a4aba653f0a107dff74739f1322dea8992300
SisolusMaximus <jaimperator99@gmail.com>
2022-03-19 12:53:17 +0100

    added sorting of cargo list

--- src/components/cargo-list/cargo-list.tsx
+++ src/components/cargo-list/cargo-list.tsx
@@ -1,39 +1,83 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { useCargoDataContext } from "../../context/cargo-data-context"
 import { useCargoWindowContext, ActionTypes as WindowActionTypes } from "../../context/cargo-window-context"
+import { ICargoData } from "../../interfaces"
 import "./cargo-list.scss"
+import { quickSort } from "./quicksort"
 
 
 const CargoList:FC =  ():JSX.Element =>{
 
+  interface IState {
+    columnName: "id" | "name",
+    descending: boolean
+  }
+
   const cargoDataContext = useCargoDataContext()
   const cargoWindowContext = useCargoWindowContext()
+  const [orderingState, setOrderingState] = useState<IState>({columnName: "id", descending:false})
 
 
   const onItemCLick = (id: number) => {
     cargoWindowContext.dispatch({type:WindowActionTypes.SHOW_SELECTED, payload: id})
-    console.log(cargoWindowContext.state)
+  }
+
+
+  const sortItems = () =>{
+    const data = cargoDataContext.state.data
+    let listOfElements:ICargoData[] = []
+    switch(orderingState.columnName){
+      case "id":
+        listOfElements = quickSort(data, 0, data.length -1, "_id")
+        break;
+      case "name":
+        listOfElements = quickSort(data, 0, data.length -1, "name")
+        break;
+      }
+      if (orderingState.descending){
+        listOfElements.reverse()
+      } 
+    return (listOfElements)
+  }
+
+
+  const renderItem =  (data:ICargoData, key:number):JSX.Element =>{
+    return(
+      <tr className={cargoWindowContext.state.selected === data._id? "selected": ""} 
+      onClick={() =>{ onItemCLick(data._id)}} key={key}>
+        <td>{data._id}</td>
+        <td>{data.name}</td>
+      </tr>
+    )
+  }
+
+
+  const renderSortedItems = ():JSX.Element => {
+    const sortedData = sortItems()
+    return(  
+    <tbody>
+      {sortedData.map( (data) => {return renderItem(data, data._id)})}
+    </tbody>
+    )
+  }
+
+  const renderList = ():JSX.Element => {
+    return(
+      <table className="table">
+        <thead>
+          <tr>
+            <th onClick={() => {setOrderingState({columnName: "id", descending: !orderingState.descending})}}>ID</th>
+            <th onClick={() => {setOrderingState({columnName: "name", descending: !orderingState.descending})}}>Name</th>
+          </tr>
+        </thead>
+        {renderSortedItems()}
+      </table>
+    )
   }
 
   return (
       <div className="cargo-list">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Name</th>
-              </tr>
-            </thead>
-              <tbody>
-                {cargoDataContext.state.data.map((val, key) => {
-                  return (
-                    <tr className={cargoWindowContext.state.selected === val._id? "selected": ""} 
-                    onClick={() =>{ onItemCLick(val._id)}} key={key}>
-                      <td>{val.name}</td>
-                    </tr>
-                  )
-                })}
-            </tbody>
-          </table>
+          {renderList()}
       </div>
   )
 }
